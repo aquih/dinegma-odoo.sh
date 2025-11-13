0d15ee0623b27c8abd425e08c0a487e538946982
Guidoom <guidoomuchiutti@gmail.com>
2025-11-13 16:47:41 -0300

    Stock barcode customization

---
+++ stock_barcode_customization/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Customizaciones Cod. de barras",
+    "version": "18.0.0.1",
+    "summary": """Customizaciones para el módulo stock_barcode""",
+    "author": "",
+    "website": "",
+    "category": "",
+    "depends": ["base", "stock_barcode"],
+    "data": [],
+    "assets": {
+        "web.assets_backend": ["stock_barcode_customization/static/src/**/*"],
+    },
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
---
+++ stock_barcode_customization/static/src/components/main_component.js
@@ -0,0 +1,37 @@
+import { patch } from "@web/core/utils/patch";
+import { download } from "@web/core/network/download";
+import { onWillStart } from "@odoo/owl";
+
+import MainComponent from "@stock_barcode/components/main"
+
+
+patch(MainComponent.prototype, {
+
+    setup() {
+        super.setup()
+        onWillStart(async () => {
+            this.user_is_admin = await this.orm.call("res.users", "is_base_admin", [], {})
+        })
+    },
+
+    onClickExportXlsx() {
+
+        const currentLines = this.env.model.currentState.lines
+
+        download({
+            url: "/custom-barcode/export_xlsx",
+            data: {
+                data: new Blob([JSON.stringify({
+                    rows: currentLines.map(l => ({
+                        id: l.id,
+                        product_id: l.product_id.id,
+                        inventory_quantity: l.inventory_quantity,
+                        quantity: l.quantity,
+                        location_id: l.location_id?.id || false,
+                        location_name: l.location_id?.display_name || "",
+                    }))
+                })], { type: "application/json" })
+            },
+        });
+    }
+})
